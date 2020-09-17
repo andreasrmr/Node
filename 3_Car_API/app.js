5920b75e1dc0e3b5817ee0bf83addc810e9b65ea
Andreas Rømer <andreasrmr@hotmail.com>
2020-09-17 10:57:30 +0200

    .

--- 3_Car_API/app.js
+++ 3_Car_API/app.js
@@ -1,15 +1,13 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 const port = 8080
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
-    res.send("Nothing to see here");
+    res.send("Nothing to see here")
 })
-
-
 
 let cars = [
     { id: "1", name: "Mazda" },
@@ -17,19 +15,19 @@ let cars = [
     { id: "7", name: "Honda"},
     { id: "8", name: "Audi"},
     { id: "9", name: "Citroen"}
-];
+]
 console.log(cars)
-let nextCarId = (Number(cars[cars.length-1].id) + 1);
+let nextCarId = (Number(cars[cars.length-1].id) + 1)
 
 function findIndex(id) {
-    let index;
+    let index
     for(let i = 0; i < cars.length; i++){
         if(cars[i].id == id){
-            index = i;
-            break;
+            index = i
+            break
         }
     }
-    return index;
+    return index
 }
 
 app.get("/cars", (req, res) => {
@@ -37,31 +35,31 @@ app.get("/cars", (req, res) => {
 })
 
 app.get("/cars/:id", (req, res) => {
-    const carId = req.params.id;
+    const carId = req.params.id
     const index = findIndex(carId)
     let car = cars[index]
-    res.send({car});
+    res.send({car})
 })
 
 app.post("/cars", (req, res) => {
-    const carId = nextCarId.toString();
-    nextCarId++;
+    const carId = nextCarId.toString()
+    nextCarId++
     const newCar = { id: carId, name: req.body.name }
-    cars.push(newCar);
+    cars.push(newCar)
     res.send({newCar})
 })
 
 app.put("/cars/:id", (req, res) => {
-    const carId = req.params.id;
-    const index = findIndex(carId);
+    const carId = req.params.id
+    const index = findIndex(carId)
     const updatedCar = { id: carId, name: req.body.name}
     if(index != undefined){
-        cars[index] = updatedCar;    
+        cars[index] = updatedCar   
     }
     else {
         cars.push(updatedCar)
     }
-    res.send({updatedCar});
+    res.send({updatedCar})
 })
 
 app.delete("/cars/:id", (req, res) => {
@@ -73,9 +71,9 @@ app.delete("/cars/:id", (req, res) => {
 
 app.listen(port, (error) => {
     if(error){
-        console.log("Something went wrong");
+        console.log("Something went wrong")
     }
-    console.log(`Server is running on port: ${port}`);
-});
+    console.log(`Server is running on port: ${port}`)
+})
 
 
